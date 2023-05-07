5e985737dc134e483579b67917e4abaf9caccd52
David Waza <wazabanga00@gmail.com>
2023-05-07 11:20:42 +0100

    created the biography page

--- src/app/biography/page.jsx
+++ src/app/biography/page.jsx
@@ -1,9 +1,28 @@
+"use client";
+import GalTributeSection from "../components/GalTributeSection";
 import Header from "../components/Header";
+import styles from "./bio.module.css";
+import { Container, Col, Row } from "react-bootstrap";
 
-export default function Biography(){
-    return(
-        <div className="text-center mt-12">
+export default function Biography() {
+  return (
+    <main>
+      <div className="text-center mt-12">
         <Header title="biography" />
       </div>
-    )
+      <div className={styles.hero}></div>
+      <Container>
+        <Row>
+          <Col>
+            <div className="mt-16">
+              <p className="primary_font secondary_font-size">Early life</p>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+            <GalTributeSection />
+        </Row>
+      </Container>
+    </main>
+  );
 }
